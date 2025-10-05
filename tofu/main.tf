617ea77145d84a961df192b3e06141ad9fb49fac
Balaji Pothula <balan.pothula@gmail.com>
2025-10-05 18:14:31 +0530

    added tofu resources

--- tofu/main.tf
+++ tofu/main.tf
@@ -1,6 +1,19 @@
 # Initialize the provider
 provider "cloudflare" {
+  api_token = var.cloudflare_api_token # The API Token for operations.
+}
 
-  api_token = var.cloudflare_api_token
-
+resource "cloudflare_dns_record" "ahooooy_dns_record" {
+  zone_id = "33939467c9e0908bbf400f4182373e2d"
+  name = "ahooooy.com"
+  ttl = 3600
+  type = "A"
+  comment = "Domain verification record"
+  content = "104.16.133.229"
+  proxied = true
+  settings = {
+    ipv4_only = true
+    ipv6_only = true
+  }
+  tags = ["owner:dns-team"]
 }
