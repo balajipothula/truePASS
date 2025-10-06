ca3b6d2d48d59fec1625c8efaf3dc11dd9330d20
Balaji Pothula <balan.pothula@gmail.com>
2025-10-06 16:22:01 +0530

    updated comment.

--- tofu/main.tf
+++ tofu/main.tf
@@ -3,9 +3,10 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-# Creation of WebApp Lambda Function AWS IAM Role.
+# Creation of truepass_d1_database.
 module "truepass_d1_database" {
   source                = "./terraform/cloudflare/cloudflare/d1_database"
+
   account_id            = var.cloudflare_account_id # 🔒 Required argument.
   name                  = "truepass_db"             # 🔒 Required argument.
   primary_location_hint = "apac"                    # ✅ Optional argument — recommended to keep.
