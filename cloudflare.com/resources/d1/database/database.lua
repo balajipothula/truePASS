ae6633619bea7dffcaddfdec907b58b4a7e3945c
Balaji Pothula <balan.pothula@gmail.com>
2025-10-28 16:06:54 +0530

    update lua code

---
+++ cloudflare.com/resources/d1/database/database.lua
@@ -0,0 +1,23 @@
+#!/bin/redbean -i
+
+# cloudflare.com
+
+local url      = arg[1]
+local method   = arg[2] or "GET"
+local req_body = arg[3] or nil
+
+local api_key = os.getenv("CLOUDFLARE_API_KEY")
+
+local status, headers, res_body = Fetch(url, {
+  method  = method,
+  body    = req_body,
+  headers = {
+    ["Content-Type"]  = "application/json",
+    ["Authorization"] = "Bearer " .. api_key,
+  }
+})
+
+local res_body_tbl, err = DecodeJson(res_body)
+
+print("status : " .. status)
+print("success: " .. tostring(res_body_tbl["success"]))
