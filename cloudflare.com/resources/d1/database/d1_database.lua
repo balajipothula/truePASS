ec98e333b9aa77fd6f9b65180492542b4b31100f
Balaji Pothula <balan.pothula@gmail.com>
2025-10-30 19:16:20 +0530

    delete_d1_db.json  added

--- cloudflare.com/resources/d1/database/d1_database.lua
+++ cloudflare.com/resources/d1/database/d1_database.lua
@@ -1,25 +1,23 @@
 #!/bin/redbean -i
 
-local config_file    = arg[1]
-local d1_db_json     = io.open(config_file, 'r')
+local config_file    = assert(arg[1], 'Usage is `redbean -i d1_database.lua action.json`')
+local d1_db_json     = assert(io.open(config_file, 'r'))
 local d1_db_json_str = d1_db_json:read('*a')
 local d1_db_tbl      = DecodeJson(d1_db_json_str)
 
 d1_db_json:close()
 
-local url          = d1_db_tbl['request']['url']
-url                = url:gsub("%$([%w_]+)", os.getenv)
-local method       = d1_db_tbl['request']['method']
-local req_body_tbl = d1_db_tbl['request']['body']
-local req_body     = EncodeJson(req_body_tbl)
-local api_key      = os.getenv("CLOUDFLARE_API_KEY")
+local url        = d1_db_tbl['request']['url']:gsub("%$([%w_]+)", os.getenv)
+local method     = d1_db_tbl['request']['method'] or 'GET'
+local req_body   = d1_db_tbl['request']['body'] and EncodeJson(d1_db_tbl['request']['body']) or nil
+local cf_api_key = os.getenv('CLOUDFLARE_API_KEY')
 
 local status, headers, res_body = Fetch(url, {
   method  = method,
   body    = req_body,
   headers = {
     ["Content-Type"]  = "application/json",
-    ["Authorization"] = "Bearer " .. api_key,
+    ["Authorization"] = "Bearer " .. cf_api_key,
   }
 })
 
