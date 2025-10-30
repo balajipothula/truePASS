0776c911414a665026d638545ce07a5fb27e1079
Balaji Pothula <balan.pothula@gmail.com>
2025-10-30 17:38:04 +0530

    json config working fine.

--- cloudflare.com/resources/d1/database/d1_database.lua
+++ cloudflare.com/resources/d1/database/d1_database.lua
@@ -1,17 +1,18 @@
 #!/bin/redbean -i
 
-local d1_db_json     = io.open('create_d1_db.json', 'r')
+local config_file    = arg[1]
+local d1_db_json     = io.open(config_file, 'r')
 local d1_db_json_str = d1_db_json:read('*a')
 local d1_db_tbl      = DecodeJson(d1_db_json_str)
 
 d1_db_json:close()
 
 local url          = d1_db_tbl['request']['url']
+url                = url:gsub("%$([%w_]+)", os.getenv)
 local method       = d1_db_tbl['request']['method']
 local req_body_tbl = d1_db_tbl['request']['body']
 local req_body     = EncodeJson(req_body_tbl)
-
-local api_key = os.getenv("CLOUDFLARE_API_KEY")
+local api_key      = os.getenv("CLOUDFLARE_API_KEY")
 
 local status, headers, res_body = Fetch(url, {
   method  = method,
@@ -22,10 +23,7 @@ local status, headers, res_body = Fetch(url, {
   }
 })
 
-print(EncodeJson(res_body))
---[[
 local res_body_tbl, err = DecodeJson(res_body)
 
 print("status : " .. status)
 print("success: " .. tostring(res_body_tbl["success"]))
-]]
